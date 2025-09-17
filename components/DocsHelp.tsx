12ae44aea3875a6c5149417dae2c8b58068d6907
Ayan Ali Khan <ayanalikhan154@gmail.com>
2025-09-17 20:35:48 +0530

    edited from optional to required

--- components/DocsHelp.tsx
+++ components/DocsHelp.tsx
@@ -237,7 +237,7 @@ export function DocsHelp({
                             Let us know your Feedback
                           </span>
                           <span className='float-right text-[#7d8590] text-[14px] block'>
-                            Optional
+                            Required
                           </span>
                         </label>
                       </p>
